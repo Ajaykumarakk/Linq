7ca91c2eb7710a25712f2e2d2499d06bbecb0428
Ajaykumarakk <Ajaykumar.kumarasamy@gmail.com>
2023-08-17 12:45:24 +0530

    Add project files.

---
+++ Linq/Exercise 1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq
+{
+    class Exercise_1
+    {
+        int[] Rank = { 23, 35, 21, 45, 60, 70 };
+
+        public void S()
+        {
+            IEnumerable<int> A = from Mark in Rank 
+                                 where Mark > 30 
+                                 select Mark;
+
+            foreach (int i in A)
+            {
+                Console.Write(i + " ");
+            }
+        }
+
+        IList<string> stringList = new List<string>()
+        {
+             "C# Tutorials",
+            "VB.NET Tutorials",
+             "Learn C++",
+             "MVC Tutorials" ,
+              "Java"
+        };
+
+        public void R()
+        {
+            var result = from S in stringList where S.Contains("Tutorials") select S;
+        }
+
+       
+
+
+    }
+}
---
+++ Linq/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Linq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Exercise_1 obj = new Exercise_1();
+            //obj.S();
+            obj.R();
+
+        }
+    }
+}
